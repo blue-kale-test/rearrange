638cb6878e362cbbd933c8afd03e427d2b23538d
Guillermo <guillermolopezziegler@gmail.com>
2022-03-10 23:53:20 -0500

    Update rearrange.py
    Crea función rearrange_name, que recibe un nombre y cambia el orden

--- rearrange.py
+++ rearrange.py
@@ -1,1 +1,9 @@
+import re
 
+def rearrange_name(name):
+    result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+    if result == None:
+        return result
+    return "{} {}".format(result[2],result[1])
+
+print(rearrange_name('Ziegler, Guillermo'))
