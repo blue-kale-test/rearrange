b4a262b7c7dcbcdb1b46548517410c480b822f99
FrankAlvaradoR <francisco.alvarado@edu.uag.mx>
2022-07-29 21:05:59 -0500

    Corregí is en lugar de es

--- rearrange.py
+++ rearrange.py
@@ -3,6 +3,6 @@ import re
 
 def rearrange_name(name):
     result = re.search(r"^([\w .]*), ([\w .]*)$", name)
-    if result es None:
+    if result is None:
         return ""
     return "{} {}".format(result[2], result[1])
