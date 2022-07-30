1960c490182dc76827230d4694c86a10a1cff325
FrankAlvaradoR <francisco.alvarado@edu.uag.mx>
2022-07-29 20:39:19 -0500

    corrijo el patrón del texto

--- rearrange.py
+++ rearrange.py
@@ -2,6 +2,5 @@
 import re
 
 def rearrange_name(name):
-    result = re.search(r"^([\w .]*), ([\w .])*$", name)
+    result = re.search(r"^([\w .]*), ([\w .]*)$", name)
     return "{} {}".format(result[2], result[1])
-    
