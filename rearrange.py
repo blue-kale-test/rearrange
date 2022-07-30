958fc016a1ea7781c02869bc6e2e1858ffbac23f
FrankAlvaradoR <francisco.alvarado@edu.uag.mx>
2022-07-29 21:04:50 -0500

    Agregamos que devuelva empty su no tiene salida

--- rearrange.py
+++ rearrange.py
@@ -3,4 +3,6 @@ import re
 
 def rearrange_name(name):
     result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+    if result es None:
+        return ""
     return "{} {}".format(result[2], result[1])
