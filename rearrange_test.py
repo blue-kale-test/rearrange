90c6c8e458b46f038148be7a9406f7e912d10246
FrankAlvaradoR <francisco.alvarado@edu.uag.mx>
2022-07-29 21:19:56 -0500

    se agregan más test

--- rearrange_test.py
+++ rearrange_test.py
@@ -13,6 +13,15 @@ class TestRearrange(unittest.TestCase):
         testcase = ""
         expected = ""
         self.assertEqual(rearrange_name(testcase), expected)
-        
+
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Voltarie"
+        expected = "Voltarie"
+        self.assertEqual(rearrange_name(testcase), expected)
 
 unittest.main()
