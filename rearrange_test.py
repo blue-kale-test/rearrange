d62ff238109da058af0802305d03a0b6bff9d975
FrankAlvaradoR <francisco.alvarado@edu.uag.mx>
2022-07-29 20:56:09 -0500

    Agregamos un primer test con Unittest

--- rearrange_test.py
+++ rearrange_test.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
 
+from rearrange import rearrange_name
+import unittest
+
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
+        testcase= "Lovelace, Ada"
+        expected= "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+unittest.main()
