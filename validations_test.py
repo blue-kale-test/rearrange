bc8b9dbe0b9858757b226a7ed01baee8704731bb
FrankAlvaradoR <francisco.alvarado@edu.uag.mx>
2022-07-31 21:29:43 -0500

    agrego una nueva función de test

---
+++ validations_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import unittest
+from validations import validate_user
+
+class TestValidateUser(unittest.TestCase):
+    def test_valid(self):
+        self.assertEqual(validate_user("valiuser",3), True)
+
+    def test_to_short(self):
+        self.assertEqual(validate_user("inv",5), False)
+
+    def test_invalid_characters(self):
+        self.assertEqual(validate_user("invalid_user",1), False)
+
+#Run the test
+unittest.main()
