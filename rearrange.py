a5a058da779d6182b0fcd55db2c42a271c117436
JohnAramenko <john.aramenko@sasktel.com>
2023-04-26 11:24:02 -0600

    Add a check_reboot function

--- rearrange.py
+++ rearrange.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+import os
+
+def check_reboot();
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exist("/run/reboot-required")
 
 def main():
     pass
