5ab74add5b802532d668cbf3b44de9f3e7f4fad1
JohnAramenko <129973524+JohnAramenko@users.noreply.github.com>
2023-04-27 14:07:46 -0600

    Update rearrange.py rebooting

--- rearrange.py
+++ rearrange.py
@@ -2,7 +2,7 @@
 import os
 
 def check_reboot();
-    """Returns True if the computer has a pending reboot."""
+    """Returns True if the computer has a pending rebooting."""
     return os.path.exist("/run/reboot-required")
 
 def main():
